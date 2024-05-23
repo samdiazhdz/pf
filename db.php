b146ad4d41ef4f8eac7db9b86caaade99cfdcae5
Sam Diaz <86027077+samdiazhdz@users.noreply.github.com>
2024-05-23 09:25:19 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,28 +1,14 @@
 <?php
-$servername = "bd-pf.mysql.database.azure.com";
+$servername = "bd-pf.privatelink.mysql.database.azure.com";
 $username = "sam";
 $password = "Bleach150!!!";
 $dbname = "estudianteDB";
-$port = 3306;
-$ssl_ca = "{path to CA cert}";
 
 // Crear la conexión
-$conn = mysqli_init();
-
-if (!$conn) {
-    die("mysqli_init failed");
-}
-
-mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
-
-if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
-    die("Conexión fallida: " . mysqli_connect_error());
-}
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
-
-echo "Conexión exitosa";
 ?>
